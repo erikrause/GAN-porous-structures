b2d7ba79ba7538e9a511c23e422a1df3b1157a9f
erikrause <erkimkamilov@gmail.com>
2019-10-24 15:49:28 +0500

    code clening in main

--- GAN-porous-structures/main.py
+++ GAN-porous-structures/main.py
@@ -12,18 +12,17 @@ if args.amd:
     os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 #################
 
-#from modules.models import base_models
 from modules.preprocess import DataLoader
 from modules.ModelHandler import ModelHandler
 import numpy as np
 
 # Start input image resolution
-channels = 1
+channels = 1        # 1 - черно-белое изображение
 start_shape = (8,8,8,channels)
-z_dim = 100
-n_blocks = 5        # End_shape = start_shape*n_blocks
+z_dim = 100         # Для сложных данных увеличить z_dim
+n_blocks = 5        # Количество повышений разрешения. End_shape = start_shape*n_blocks
 
-# Filters for each resolution block:
+# Filters for each resolution block (from hidden layers to end resolution layers):
 n_filters = {1: 64,
              2: 32,
              3: 16,
@@ -37,26 +36,25 @@ filter_sizes = {1: 3,
 # 'directory_name/' (слэш только в конце названия папки!).
 DIRECTORY = ''
 #DATASET_DIR = 'datasets/berea/{}.png'  -   for png files iteration
-DATASET_DIR = 'datasets/beadpack/beadpack.tif'
+DATASET_DIR = 'datasets/beadpack/{}.png'
+is_tif = False      # Change to false for downloading .png files
+
 # Initialize dataset:
-
 img_dims = len(start_shape) - 1
-data_loader = DataLoader(DATASET_DIR, (500, 500, 500), is_tif=True, dims=img_dims)
-  
+data_loader = DataLoader(DATASET_DIR, (500, 500, 500), is_tif=is_tif, dims=img_dims)
 
-# Build a models (если модели и логи лежат в папке History, то будут загружены с диска):
+# Build a models (если логи лежат в папке History, то веса моделей будут загружены с папки History/models_wetights):
 WEIGHTS_DIR = 'models-custom/'
 model_handler = ModelHandler(DIRECTORY, start_shape, z_dim, n_blocks,  n_filters, filter_sizes, data_loader)#, WEIGHTS_DIR)
 
-
 ######################################
-# MAIN LOOPv7
+# MAIN LOOP
 ######################################
 
-batch_size = 16
+batch_size = 64
 sample_interval = 100    # должно быть кратно итерациям
 # Итерации на каждый слой:
 n_fadein = np.array([0, 2500, 2000, 2000, 2000])
-n_straight = np.array([4000-4000, 4000, 3000, 3000, 3000])
+n_straight = np.array([4000, 4000, 3000, 3000, 3000])
 
 model_handler.train(n_straight, n_fadein, batch_size, sample_interval)
