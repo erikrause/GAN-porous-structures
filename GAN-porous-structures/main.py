f84a6ab2236dbd6095a7a32842c6e1cf67d8c1e0
erikrause <erkimkamilov@gmail.com>
2019-11-02 05:45:19 +0500

    delete old 3D files

--- GAN-porous-structures/main.py
+++ GAN-porous-structures/main.py
@@ -19,7 +19,7 @@ import numpy as np
 # Start input image resolution
 channels = 1        # 1 - черно-белое изображение
 start_shape = (16,16,16,channels)
-z_dim = 100         # Для сложных данных увеличить z_dim
+z_dim = 200         # Для сложных данных увеличить z_dim
 n_blocks = 4        # Количество повышений разрешения. End_shape = start_shape*n_blocks
 is_nearest = False   #
 
@@ -37,12 +37,12 @@ filter_sizes = {1: 3,
 # 'directory_name/' (слэш только в конце названия папки!).
 DIRECTORY = ''
 #DATASET_DIR = 'datasets/berea/{}.png'  -   for png files iteration
-DATASET_DIR = 'datasets/beadpack/beadpack.tif'
+DATASET_DIR = 'datasets/berea/berea.tif'
 is_tif = True      # Change to false for downloading .png files
 
 # Initialize dataset:
 img_dims = len(start_shape) - 1
-data_loader = DataLoader(DATASET_DIR, (496, 496, 496), n_blocks, is_tif=is_tif, dims=img_dims, is_nearest_batch=is_nearest)
+data_loader = DataLoader(DATASET_DIR, (400, 400, 400), n_blocks, is_tif=is_tif, dims=img_dims, is_nearest_batch=is_nearest)
 
 # Build a models (если логи лежат в папке History, то веса моделей будут загружены с папки History/models_wetights):
 WEIGHTS_DIR = 'models-custom/'
@@ -52,7 +52,7 @@ model_handler = ModelHandler(DIRECTORY, start_shape, z_dim, n_blocks, n_filters,
 # MAIN LOOP
 ######################################
 
-batch_size = 10
+batch_size = 32
 sample_interval = 100    # должно быть кратно итерациям
 # Итерации на каждый слой:
 n_fadein = np.array([0, 5000, 4000, 4000, 4000])
