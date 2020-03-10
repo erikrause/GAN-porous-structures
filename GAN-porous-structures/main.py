WGAN_GP implementation end. Doesnt work with PlaidML (gradient calculation bug)

GAN-porous-structures/main.py
@@ -57,7 +57,7 @@ model_handler = ModelHandler(DIRECTORY, start_shape, z_dim, n_blocks, n_filters,
 ######################################
 
 batch_size = 16
-sample_interval = 100    # должно быть кратно итерациям
+sample_interval = 1    # должно быть кратно итерациям
 # Итерации на каждый слой:
 #n_fadein = np.array([0, 2500, 3500, 10000, 14000])
 #n_straight = np.array([3400, 6000, 30000, 80000, 200000])