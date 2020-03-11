08ef6b1263db1da03e1fef7432afd991c392c5b1
Эркин Камилов <erkimkamilov@gmail.com>
2020-03-11 18:50:37 +0500

    main parameters edit for jupyter

--- GAN-porous-structures/main.py
+++ GAN-porous-structures/main.py
@@ -1,18 +1,32 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-amd', action='store_true', help='Enable PlaidML backend (for AMD devices)')
-args = parser.parse_args()
+import sys
+if 'plaidml' in sys.modules:
 
-#################
-#ENABLE PLAIDML FOR AMD DEVICES:
-if args.amd:
-    import os
-    import plaidml
-    import plaidml.keras
-    plaidml.keras.install_backend()
-    os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
-    #import keras
-#################
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-amd', action='store_true', help='Enable PlaidML backend (for AMD devices)')
+    args = parser.parse_args()
+
+    #################
+    #ENABLE PLAIDML FOR AMD DEVICES:
+    if args.amd:
+        import os
+        import plaidml
+        import plaidml.keras
+        plaidml.keras.install_backend()
+        os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+        #import keras
+    #################
+
+#-----------------------------
+# Jupyter notebook starts here:
+#-----------------------------
+import sys
+sys.path.append(DIRECTORY)
+# 'directory_name/' (слэш только в конце названия папки!).
+DIRECTORY = ''
+#DATASET_DIR = 'datasets/berea/{}.png'  -   for png files iteration
+DATASET_DIR = 'datasets/berea/berea.tif'
+is_tif = True      # Change to false for downloading .png files
 
 from modules.preprocess import DataLoader
 from modules.ModelHandler import ModelHandler
@@ -39,20 +53,19 @@ n_filters = [ 64,   # 8x8x8     - example of resolution
               4 ]   # 128x128x128
 
 filters_sizes = [ 3,     # 8x8x8     # TODO: попробовать большой фильтр
-                 3,     # 16x16x16
-                 3,     # 32x32x32
-                 3,     # 64x64x64
-                 3]     # 128x128x128
-
-# 'directory_name/' (слэш только в конце названия папки!).
-DIRECTORY = ''
-#DATASET_DIR = 'datasets/berea/{}.png'  -   for png files iteration
-DATASET_DIR = 'datasets/beadpack/beadpack.tif'
-is_tif = True      # Change to false for downloading .png files
+                  3,     # 16x16x16
+                  3,     # 32x32x32
+                  3,     # 64x64x64
+                  3]     # 128x128x128
 
 # Initialize dataset:
 img_dims = len(start_shape) - 1
-data_loader = DataLoader(DATASET_DIR, (496,466,496), n_blocks, is_tif=is_tif, dims=img_dims, is_nearest_batch=is_nearest)
+
+# Нужно чтобы dataset_shape делилось на 2^n_blocks-1 без остатка (обрезать датасет, если необходимо)
+#dataset_shape = (496,466,496)   # for beadpack, original - (500, 500, 500)
+dataset_shape = (400,400,400)   # for berea
+#dataset_shape = (256,256,256)   # for ketton
+data_loader = DataLoader(DATASET_DIR, dataset_shape, n_blocks, is_tif=is_tif, dims=img_dims, is_nearest_batch=is_nearest)
 
 # Build a models (если логи лежат в папке History, то веса моделей будут загружены с папки History/models_wetights):
 WEIGHTS_DIR = 'models-custom/'  # не используется
@@ -64,7 +77,6 @@ base_models.n_filters = n_filters
 base_models.filters_sizes = filters_sizes
 base_models.conv_per_res = 1
 # /
-
 
 # Setting hyperparameters/
 batch_size = 32
@@ -86,11 +98,7 @@ sample_interval = 1    # должно быть кратно итерациям
 
 model_handler = ModelHandler(DIRECTORY, start_shape, z_dim, n_blocks, n_filters, filters_sizes, data_loader)#, WEIGHTS_DIR)
 # Итерации на каждый слой:
-#n_fadein = np.array([0, 2500, 3500, 10000, 14000])
-#n_straight = np.array([3400, 6000, 30000, 80000, 200000])
-#n_fadein = np.array([0, 5000, 5000, 50000, 14000])
-#n_straight = np.array([0, 5900, 50000, 80000, 200000])
-n_fadein = np.array([0, 0,0,0,0,0])
-n_straight = np.array([0, 0,0,0,0,0,0])
+n_fadein = np.array([0, 4000, 4000, 4000, 4000])
+n_straight = np.array([8000, 12000, 17000, 20000, 200000])
 
 model_handler.train(n_straight, n_fadein, batch_size, sample_interval)
